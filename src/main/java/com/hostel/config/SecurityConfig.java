d1de392156ef93645e4d0a9d44a4e265333739c3
vinnakotalokesh <vinnakotalokeshgufus2024@gmail.com>
2026-06-04 23:36:36 -0400

    Update SecurityConfig.java

--- src/main/java/com/hostel/config/SecurityConfig.java
+++ src/main/java/com/hostel/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.hostel.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -40,8 +41,12 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth ->
                         auth
+                                .requestMatchers(HttpMethod.OPTIONS, "/**")
+                                .permitAll()
+
                                 .requestMatchers("/auth/**")
                                 .permitAll()
+
                                 .anyRequest()
                                 .authenticated()
                 )
